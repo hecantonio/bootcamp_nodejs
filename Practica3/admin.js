a6487b1219ff0d9a1b9c02dd8831492a7a099989
gugucast02 <you@example.com>
2023-06-17 12:09:58 -0500

    practica3

--- Practica3/admin.js
+++ Practica3/admin.js
@@ -12,18 +12,12 @@ const AdminBookings = require('./controllers/admin/bookings')
 
 app.use(bodyParser.json({ type: 'application/json' }))
 
-const middFecha = (req, res, next) => {
-    const date = new Date();
-    console.log('Time: ' + date);
-    next();
-}
 
 app.use(session({
     secret: 'Thiismypassword',
     cookie: { maxAge: 60000 }
 }))
 
-app.use(middFecha);
 
 app.engine('ejs', engine);
 app.set('views', __dirname + '/views');
