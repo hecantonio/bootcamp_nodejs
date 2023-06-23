83653db1e6d2aa10e3dd65745d22ff561bf1ffee
Hector Jimenez <hec.zenemij@gmail.com>
2023-06-22 22:34:40 -0500

    se corrige al realizar delete bookings _id

--- Practica2/db.js
+++ Practica2/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://leonardolarrea1:RfII1K1caoBJYJVr@cluster0.j3g4xyz.mongodb.net/BootCampDB?retryWrites=true&w=majority')
+
+//const URL = 'mongodb+srv://leonardolarrea1:RfII1K1caoBJYJVr@cluster0.j3g4xyz.mongodb.net/BootCampDB?retryWrites=true&w=majority';
+const URL = 'mongodb+srv://bootcamp:bootcamp@cluster0.beimaw4.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(URL)
 .then(() => {
     console.log('Database connected!');
 })
